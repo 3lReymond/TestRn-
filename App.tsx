db9d22aae2ddc0118bcc28c60317045467337ff5
3lReymond <ramonbatidas@gmail.com>
2025-05-16 13:29:58 -0700

    Primer commit - proyecto React Native con fondo de imagen

--- App.tsx
+++ App.tsx
@@ -1,131 +1,148 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  FlatList,
+  ImageBackground,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
-  return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  fondo: {
+    flex: 1,
+    resizeMode: 'cover',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)', // fondo semi-transparente
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  title: {
+    fontSize: 20,
+    color: '#fff',
   },
-  highlight: {
-    fontWeight: '700',
+  text: {
+    fontSize: 16,
+    color: '#fff',
+  },
+  textDone: {
+    fontSize: 16,
+    color: '#d3d3d3',
+    textDecorationLine: 'line-through',
+  },
+  whitetext: {
+    fontSize: 16,
+    color: '#FFF',
+  },
+  TextInput: {
+    borderColor: '#6f6f6f',
+    borderWidth: 1,
+    width: Dimensions.get('screen').width * 0.6,
+    borderRadius: 15,
+    paddingLeft: 15,
+    backgroundColor: '#fff',
+  },
+  inpuntContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  addButton: {
+    width: Dimensions.get('screen').width * 0.25,
+    backgroundColor: '#5897fb',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  scrollContainer: {
+    marginTop: 30,
+  },
+  itemContainer: {
+    paddingVertical: 20,
+    borderBottomColor: '#6f6f6f',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  removeButton: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    borderRadius: 10,
   },
 });
 
-export default App;
+const tasks = [
+  {
+    title: 'Mandar los avances',
+    done: false,
+    date: new Date(),
+  },
+  {
+    title: 'Salir a correr',
+    done: false,
+    date: new Date(),
+  },
+  {
+    title: 'Nueva Tarea',
+    done: true,
+    date: new Date(),
+  },
+];
+
+interface Task {
+  title: string;
+  done: boolean;
+  date: Date;
+}
+
+export default function App() {
+  function renderItem({ item }: { item: Task }) {
+    return (
+      <View style={styles.itemContainer}>
+        <TouchableOpacity>
+          <Text style={item.done ? styles.textDone : styles.text}>
+            {item.title}
+          </Text>
+          <Text style={item.done ? styles.textDone : styles.text}>
+            {item.date ? item.date.toLocaleDateString() : 'Sin fecha'}
+          </Text>
+        </TouchableOpacity>
+        {item.done && (
+          <TouchableOpacity style={styles.removeButton}>
+            <Text style={styles.whitetext}>Eliminar</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }
+
+  return (
+    <ImageBackground
+      source={require('./ing/RN_ing.jpg')}
+      style={styles.fondo}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Mis pendientes por hacer</Text>
+
+        <View style={styles.inpuntContainer}>
+          <TextInput
+            placeholder="Agregar una nueva tarea"
+            placeholderTextColor="#6f6f6f"
+            style={styles.TextInput}
+          />
+          <TouchableOpacity style={styles.addButton}>
+            <Text style={styles.whitetext}>Agregar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.scrollContainer}>
+          <FlatList renderItem={renderItem} data={tasks} />
+        </View>
+      </View>
+    </ImageBackground>
+  );
+}
